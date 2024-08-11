8f257c55d2513a0774b6dd3e26dc99d3f5a8beab
Sheral Dsouza <sheraldsouza57@gmail.com>
2024-08-12 00:01:01 +0530

    Added images of products, icons, fonts and colors.

---
+++ lib/Resources/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors {
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+  static const blue = Color(0xFF3669C9);
+  static const charcoal = Color(0xFF0C1A30);
+  static const davisGrey = Color(0xFFC4C5C4);
+  static const featherGrey = Color(0xFFEDEDED);
+  static const darkGrey = Color(0xFF393F42);
+}
---
+++ lib/Resources/app_icons.dart
@@ -0,0 +1,22 @@
+class AppIcons {
+  static const _prefix = "assets/icons/";
+  static const account = "${_prefix}account.png";
+  static const activeAccount = "${_prefix}active_account.png";
+  static const activeHistory = "${_prefix}active_history.png";
+  static const activeHome = "${_prefix}active_home.png";
+  static const activeWishlist = "${_prefix}active_wishlist.png";
+  static const cart = "${_prefix}cart.png";
+  static const fashion = "${_prefix}fashion.png";
+  static const foods = "${_prefix}foods.png";
+  static const gadget = "${_prefix}gadget.png";
+  static const gift = "${_prefix}gift.png";
+  static const history = "${_prefix}home.png";
+  static const home = "${_prefix}account.png";
+  static const notification = "${_prefix}history.png";
+  static const wishlist = "${_prefix}wishlist.png";
+  static const bag = "${_prefix}bag.png";
+  static const rating = "${_prefix}rating.png";
+  static const activeLike = "${_prefix}active_like.png";
+  static const like = "${_prefix}like.png";
+  static const back = "${_prefix}back.png";
+}
